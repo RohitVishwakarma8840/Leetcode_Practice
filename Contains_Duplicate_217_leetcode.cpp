5b0663526bd9fc5f6f1cf41d381ecea476fe9fe7
Rohit Vishwakarma <rohit8840@gmail.com>
2024-05-29 11:44:15 +0530

    adding code

---
+++ Contains_Duplicate_217_leetcode.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+  int n=nums.size();
+  sort(nums.begin(),nums.end());
+
+  if(n==0)
+  return false;
+
+ for(int i=1;i<n;i++){
+
+ if(nums[i]==nums[i-1])
+ return true;
+
+
+
+ }
+
+return false;
+
+        
+    }
+};
