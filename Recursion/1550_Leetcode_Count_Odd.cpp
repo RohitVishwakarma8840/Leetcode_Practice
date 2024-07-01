cd980451d500633cf5617655bae6c3e17d847eef
Rohit Vishwakarma <rohit8840@gmail.com>
2024-07-01 09:19:37 +0530

    adding code

---
+++ Recursion/1550_Leetcode_Count_Odd.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int count =0;
+    bool threeConsecutiveOdds(vector<int>& arr) {
+
+        for(int i=0;i<arr.size();i++){
+
+    if(arr[i]%2!=0){
+        count++;
+        if(count==3)
+        return true;
+    }
+    else{
+        count=0;
+    }
+
+
+        }
+        
+
+        return false;
+        
+    }
+};
